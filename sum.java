62d42e7eaa1206af705db6556d51fffe5b858ce5
jadhavabhiee <92208809+jadhavabhiee@users.noreply.github.com>
2021-10-10 22:16:15 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,14 @@
+public class sum {
+    
+	public static void main(String []args)
+	{
+		int i=1,sum=0;
+		while(i<=200)
+		{
+			sum+=i;
+			i++;
+		}
+		System.out.println("Sum is "+sum);
+	}
+}
+
